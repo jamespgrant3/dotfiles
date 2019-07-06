c4c344c14f45f3a04116f6d4766c47a0a174e334
James Grant <jamespgrant3@gmail.com>
2019-07-06 00:50:39 -0400

    added tmux yml files

--- bu.sh
+++ bu.sh
@@ -3,7 +3,7 @@ cp ~/.vimrc ~/repos/dotfiles/.vimrc
 cp ~/.tmux.conf ~/repos/dotfiles/.tmux.conf
 cp ~/.bash_profile ~/repos/dotfiles/bash_profile
 cp ~/.config/coc/extensions/package.json ~/repos/dotfiles/coc_package.json
-cp ~/.config/tmuxinator/ ~/repos/dotfiles/
+cp -a ~/.config/tmuxinator/. ~/repos/dotfiles/
 git add .
 git commit
 git push
