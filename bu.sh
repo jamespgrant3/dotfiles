b9d74a853c333651013852bb93243320b4ed9249
James Grant <jamespgrant3@gmail.com>
2019-07-06 00:53:36 -0400

    restructure

--- bu.sh
+++ bu.sh
@@ -2,8 +2,8 @@ cp ~/.zshrc ~/repos/dotfiles/.zshrc
 cp ~/.vimrc ~/repos/dotfiles/.vimrc 
 cp ~/.tmux.conf ~/repos/dotfiles/.tmux.conf
 cp ~/.bash_profile ~/repos/dotfiles/bash_profile
-cp ~/.config/coc/extensions/package.json ~/repos/dotfiles/coc_package.json
-cp -a ~/.config/tmuxinator/. ~/repos/dotfiles/
+cp ~/.config/coc/extensions/package.json ~/repos/dotfiles/coc/extensions/package.json
+cp -a ~/.config/tmuxinator/. ~/repos/dotfiles/tmuxinator/
 git add .
 git commit
 git push
