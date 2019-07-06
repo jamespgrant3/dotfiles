0e14cf678db412c200d90d28f811a20df5712b0b
James Grant <jamespgrant3@gmail.com>
2019-07-06 01:41:52 -0400

    added more files

--- bu.sh
+++ bu.sh
@@ -1,7 +1,10 @@
 cp ~/.zshrc ~/repos/dotfiles/.zshrc
+cp ~/.bashrc ~/repos/dotfiles/.bashrc
+cp ~/.gitconfig ~/repos/dotfiles/git/.gitconfig
 cp ~/.vimrc ~/repos/dotfiles/.vimrc 
 cp ~/.tmux.conf ~/repos/dotfiles/.tmux.conf
 cp ~/.bash_profile ~/repos/dotfiles/bash_profile
+cp ~/.vim/coc-settings.json ~/repos/dotfiles/coc/coc-settings.json
 cp ~/.config/coc/extensions/package.json ~/repos/dotfiles/coc/extensions/package.json
 cp -a ~/.config/tmuxinator/. ~/repos/dotfiles/tmuxinator/
 git add .
