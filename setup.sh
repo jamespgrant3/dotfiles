d4de2c8a719a33a2b908d060bd022669ce1a0b22
James Grant <jamespgrant3@gmail.com>
2025-02-03 21:58:30 -0500

    updated setup, added config for lazydocker

--- setup.sh
+++ setup.sh
@@ -18,13 +18,13 @@ ln -s $HOME/repos/dotfiles/master/.zprofile $HOME/.zprofile
 
 ln -s $HOME/repos/dotfiles/master/.client $HOME/.client
 
-# window tooling
-ln -s $HOME/repos/dotfiles/master/.skhdrc $HOME/.skhdrc
-ln -s $HOME/repos/dotfiles/master/.yabairc $HOME/.yabairc
-
-# lazy git
+# lazygit
 mkdir -p /Users/james/.config/lazygit
 ln -s $HOME/repos/dotfiles/master/.config/lazygit/config.yml $HOME/.config/lazygit/config.yml
+
+# lazydocker
+mkdir -p /Users/james/.config/lazydocker
+ln -s $HOME/repos/dotfiles/master/.config/lazydocker/config.yml $HOME/.config/lazydocker/config.yml
 
 # yazi - file explorer
 ln -s $HOME/repos/dotfiles/master/.config/yazi $HOME/.config/yazi
